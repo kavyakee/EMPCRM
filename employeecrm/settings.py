d4b0e824419a7cc670b284275cd008e666976304
kavya <kavyak000333@gmail.com>
2023-11-17 13:42:22 +0530

    new added

--- employeecrm/settings.py
+++ employeecrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(f^9^4^yd$l-#ed6&!a*gi&@vsb8nf7t(ik=uq3unq#^9hh0c4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
